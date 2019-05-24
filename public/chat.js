9a2c8970dc6676178ecdf1135e22921e4749688c
cnrd <hello@conradweise.com>
2019-05-24 13:45:25 +0200

    refactor to vue

--- public/chat.js
+++ public/chat.js
@@ -1,73 +1,50 @@
-const WS_URL = window.location.hostname === 'localhost' ? 'ws://localhost:5001' : 'wss://translation-of-computation.com/ws'
-const socket = new WebSocket(WS_URL)
+const app = new Vue({
+  el: '#app',
+  data: {
+    socket: null,
+    username: Math.random().toString(16).slice(2),
+    message: '',
+    info: 'There are no more of these HITs available.',
+    bubbles: []
+  },
+  created: function () {
+    const WS_URL = window.location.hostname === 'localhost' ? 'ws://localhost:5001' : 'wss://translation-of-computation.com/ws'
+    this.socket = new WebSocket(WS_URL)
 
-const chat = document.getElementById('chat')
-const bubbles = document.getElementById('bubbles')
-const textareaMessage = document.getElementById('textarea-message')
-const inputUsername = document.getElementById('input-username')
-const buttonSend = document.getElementById('button-send')
-const overlay = document.getElementById('overlay')
-const info = document.getElementById('info')
-
-const usernameId = Math.random().toString(16).slice(2)
-
-function send() {
-  const msg = {
-    type: 'chat-message',
-    message: textareaMessage.value.trim(),
-    username: usernameId,
-    timestamp: Date.now()
-  }
-
-  socket.send(JSON.stringify(msg))
-  textareaMessage.value = ''
-}
-
-function getBubble (username, message, timestamp) {
-  const self = (username === usernameId) ? 'self' : ''
-  const date = new Date(timestamp)
-  const time = `${ date.getDate() }.${ date.getMonth() + 1}.${ date.getFullYear() } ${ ('0' + date.getHours()).slice(-2) }:${ ('0' + date.getMinutes()).slice(-2) }`
-
-  return bubble = `
-    <section class="bubble-container ${ self }">
-      <section class="bubble ${ self }">
-        <p class="message">${ message.replace(/<\/?[^>]+(>|$)/g, '') }</p>
-        <p class="timestamp ${ self }">${ time }</p>
-      </section>
-    </section>`
-}
-
-function insert (username, message, timestamp) {
-  const bubble = getBubble(username, message, timestamp)
-
-  bubbles.insertAdjacentHTML('beforeend', bubble)
-  bubbles.scrollTop = bubbles.scrollHeight
-}
-
-socket.addEventListener('message', (event) => {
-  const msg = JSON.parse(event.data)
-
-  if (msg.type === 'chat-message') {
-    insert(msg.username, msg.message, msg.timestamp)
-  } else if (msg.type === 'hello') {
-    chat.style.display = 'block'
-    overlay.style.display = 'none'
-  } else if (msg.type === 'exit') {
-    chat.style.display = 'none'
-    info.innerHTML = `Person left the conversation.<br>This is your survey code: <mark>${ usernameId }</mark>`
-    overlay.style.display = 'flex'
-  }
-})
-
-buttonSend.addEventListener('click', () => {
-  if (!textareaMessage.value.trim()) return
-  send()
-})
-
-textareaMessage.addEventListener('keydown', (event) => {
-  if (event.keyCode === 13) {
-    event.preventDefault()
-    if (!textareaMessage.value.trim()) return
-    send()
+    this.socket.addEventListener('message', (event) => {
+      const msg = JSON.parse(event.data)
+    
+      if (msg.type === 'chat-message') {
+        this.insert(msg.username, msg.message, msg.timestamp)
+      } else if (msg.type === 'hello') {
+        this.info = null
+      } else if (msg.type === 'exit') {
+        this.info = `Person left the conversation.<br>This is your survey code: <mark>${ this.username }</mark>`
+      }
+    })
+  },
+  methods: {
+    send: function () {
+      const msg = {
+        type: 'chat-message',
+        message: this.message.trim(),
+        username: this.username,
+        timestamp: Date.now()
+      }
+    
+      this.socket.send(JSON.stringify(msg))
+      this.message = ''
+    },
+    insert: function (username, message, timestamp) {
+      this.bubbles.push({ username, message, timestamp })
+      this.$refs.bubbles.scrollTop = bubbles.scrollHeight
+    },
+    self: function (username) {
+      return username === this.username
+    },
+    date: function (timestamp) {
+      const date = new Date(timestamp)
+      return `${ date.getDate() }.${ date.getMonth() + 1}.${ date.getFullYear() } ${ ('0' + date.getHours()).slice(-2) }:${ ('0' + date.getMinutes()).slice(-2) }`
+    }
   }
 })
